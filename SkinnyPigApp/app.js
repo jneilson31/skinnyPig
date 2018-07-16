98e6e4e2a94992d47d42c5525850a41a0c4fb57d
Trenton Neilson <trentonneilson14@hotmail.com>
2018-07-16 13:19:22 -0600

    get categories working

--- SkinnyPigApp/app.js
+++ SkinnyPigApp/app.js
@@ -21,13 +21,59 @@ app.get("/", function (req, res) {
 
 });
 
-app.get("/clothing", function (req, res) {
+app.get("/category/:categoryName", function (req, res) {
     var request = require('request');
-    request('http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByCategory&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=TrentonN-SkinnyPi-PRD-bc970d9ce-9ed166d5&version=517&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&categoryId=11450&paginationInput.entriesPerPage=12&itemFilter(0).name=ListingType%20&itemFilter(0).value(0)=AuctionWithBIN&itemFilter(0).value(1)=FixedPrice&itemFilter(0).value(2)=StoreInventory', function (error, response, body) {
+    var catID;
+
+switch(req.params.categoryName) {
+    case "video-games":
+        catID = 1249;
+    break;
+    case "tv":
+        catID = 11071;
+    break;
+    case "computers":
+        catID = 58058;
+    break;
+    case "outdoors":
+        catID = 159043;
+    break;
+    case "home":
+        catID = 11700;
+    break;
+    case "apparel":
+        catID = 11450;
+    break;
+    case "technology":
+        catID = 293;
+    break;
+    case "cameras":
+        catID = 625;
+    break;
+    case "auto":
+        catID = 6000;
+    break;
+    case "health-and-beauty":
+        catID = 26395;
+    break;
+    case "children":
+        catID = 2984;
+    break;
+    case "entertainment":
+        catID = 888;
+    break;
+    case "travel":
+        catID = 3252;
+    break;
+    default:
+        catID = 99;
+}
+
+    request('http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByCategory&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=TrentonN-SkinnyPi-PRD-bc970d9ce-9ed166d5&version=517&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&categoryId=' + catID + '&paginationInput.entriesPerPage=24&itemFilter(0).name=ListingType%20&itemFilter(0).value(0)=AuctionWithBIN&itemFilter(0).value(1)=FixedPrice&itemFilter(0).value(2)=StoreInventory', function (error, response, body) {
 
         if (!error && response.statusCode === 200) {
             var data = JSON.parse(body);
-            res.render("clothing", { data: data });
+            res.render("category", { data: data });
         }
 
     });
